e1878f75abd8c515514ac5077b87ddde7e965aee
jordanvieira <marvaojordan98@gmail.com>
2022-02-17 11:54:19 -0300

    DevOps lab

---
+++ vagrant-lab/.vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.30.0"]]],"checksum":"11b1b35cf79113cc49e78dfeea4aabc6746d1df7877ec4f5145f0b7b96ad3af1","vagrant_version":"2.2.18"}
