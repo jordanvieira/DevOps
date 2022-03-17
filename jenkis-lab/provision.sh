e2c0f347f535c9b1c759abfa5442e89ec70828d2
jordanvieira <marvaojordan98@gmail.com>
2022-03-17 14:07:31 -0300

    jenkins-lab

---
+++ jenkis-lab/provision.sh
@@ -0,0 +1,23 @@
+#/bin/bash
+yum install epel-release -y 
+yum install wget -y
+
+sudo wget --no-check-certificate -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+yum install java-11-openjdk-devel -y
+yum install jenkins -y
+systemctl daemon-reload
+service jenkins start
+
+
+#### instalação docker e docker compose
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce docker-ce-cli  containerd.io -y
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+systemctl daemon-reload
+systemctl restart docker
