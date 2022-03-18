0361c8b6dfedee2ba53c621c0ad267d1851b63eb
jordanvieira <marvaojordan98@gmail.com>
2022-03-17 21:35:15 -0300

    jenkins-lab

--- jenkins-lab/provision.sh
+++ jenkins-lab/provision.sh
@@ -22,3 +22,14 @@ sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 systemctl daemon-reload
 systemctl restart docker
 usermod -aG docker jenkins
+
+##instalaçãosonar scanner
+sudo su
+yum install wget unzip -y
+wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.6.2.2472-linux.zip
+unzip sonar-scanner-cli-4.6.2.2472-linux.zip -d /opt/
+mv /opt/sonar-scanner-4.6.2.2472-linux /opt/sonar-scanner
+chown -R jenkins:jenkins /opt/sonar-scanner
+echo 'export PATH=$PATH:/opt/sonar-scanner/bin' | sudo tee -a /etc/profile
+curl -sL https://rpm.nodesource.com/setup_10.x | sudo bash -
+sudo yum install nodejs -y
